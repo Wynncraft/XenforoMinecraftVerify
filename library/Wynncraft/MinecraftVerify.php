7fce848d8387baa81fa62a92f603e61556a51e23
Chris Ireland <ireland63@googlemail.com>
2015-02-21 10:27:53 +0000

    fix syntax errors

--- library/Wynncraft/MinecraftVerify.php
+++ library/Wynncraft/MinecraftVerify.php
@@ -1,14 +1,14 @@
 <?php
 
-class Wynncraft_Minecraft Verify
+class Wynncraft_MinecraftVerify
 {
   public static function validate($field, &$value, &$error)
   {
     // Check the haspaid api to see if the minecraft player is valid
     $ch = curl_init('https://minecraft.net/haspaid.jsp?user=' . $value);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, treu);
-    $response = curl_exec($ch)
-    curl_close($ch)
+    $response = curl_exec($ch);
+    curl_close($ch);
     
     // Then return the results
     if($response == "true") {
